bacbb7dfe72ddfd1f9b00e85525821ee7cb654b7
WareeshaMaryam <wareeshamaryam8@gmail.com>
2024-08-12 00:11:50 +0500

    Done

---
+++ script.js
@@ -0,0 +1,23 @@
+Declare 3 variables in one statement
+let age = quantity = number = 46;
+
+
+
+
+
+Legal Variable Names:
+
+firstName
+_super
+$price
+vanish2
+camelCaseName
+
+Illegal Variable Names:
+
+
+3smart(Cannot start with a number)
+first-name (Hyphens are not allowed)
+var (Cannot use reserved keywords)
+@name (Special characters like @ are not allowed)
+class name (Spaces are not allowed)
